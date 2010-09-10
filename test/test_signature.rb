8ff2e8000a237e434f36944d523390e4145fee43
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2010-09-09 21:06:14 -0500

    Initial commit

---
+++ test/test_signature.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby -w
+$LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
+require "test/unit"
+require "portable_hole"
+
+class TestPortableHole < Test::Unit::TestCase
+  def setup
+    @access_key = "0PN5J17HBGZHT7JJ3X82"
+    @secret_key = "uV3F3YluFJax1cknvbcGwgjvx4QpvB+leU8dUj2o"
+    @date = 'Tue, 27 Mar 2007 19:36:42 +0000'
+    @request_type = 'GET'
+    @canonical_resource = '/johnsmith/photos/puppy.jpg'
+  
+    @ph = PortableHole.new(@access_key, @secret_key)
+  end
+  
+  def test_auth_header_string_to_sign
+    expected =<<STRING.chomp
+GET
+
+
+Tue, 27 Mar 2007 19:36:42 +0000
+/johnsmith/photos/puppy.jpg
+STRING
+    
+    
+    actual = @ph.string_to_sign(@date, @request_type, @canonical_resource)
+    
+    assert_equal expected, actual
+  end
+  
+  def test_auth_header
+    expected = "Authorization: AWS 0PN5J17HBGZHT7JJ3X82:xXjDGYUmKxnwqr5KXNPGldn5LbA="
+
+    actual = @ph.sign(@date, @request_type, @canonical_resource)
+    
+    assert_equal expected, actual
+  end
+  
+  # someday/maybe features
+  # test_lowercase_request_type
+  # test_get
+end
