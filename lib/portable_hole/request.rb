a2a5f13811d2c28d880d289eedb19791725ca191
Chad Gorshing <gorshing@gmail.com>
2010-12-13 22:32:58 -0600

    Working on the new project layout

---
+++ lib/portable_hole/request.rb
@@ -0,0 +1,30 @@
+require 'openssl'
+require 'base64'
+
+module PortableHole
+  class Request 
+
+    def initialize(access_key, secret_key)
+      @access_key = access_key
+      @secret_key = secret_key
+    end
+
+    def string_to_sign(date, request_type, resource, content_type)
+      request_type + "\n" +
+      # content_md5 +
+      "\n" +
+      content_type + "\n" +
+      date + "\n" +
+      # canonicalized_amz_headers +
+      resource
+    end
+
+    def sign(date, request_type, resource)
+      raw_hmac = OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), @secret_key, string_to_sign(date, request_type, resource))
+      sig = Base64.encode64(raw_hmac).chomp
+
+      "Authorization: AWS %s:%s" % [@access_key, sig]
+    end
+
+  end
+end
