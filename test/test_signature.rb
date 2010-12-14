97fb3fe4dabb127c61dd0bee7bffacc5b1e46d0e
Chad Gorshing <gorshing@gmail.com>
2010-12-13 22:27:50 -0600

    Reworking the project layout

--- test/test_signature.rb
+++ test/test_signature.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/env ruby -w
 
 require "test/unit"
-require "portable_hole"
+require "portable_hole/request"
 
-class TestPortableHole < Test::Unit::TestCase
+class TestPortableHoleRequest < Test::Unit::TestCase
   def setup
     @access_key = "0PN5J17HBGZHT7JJ3X82"
     @secret_key = "uV3F3YluFJax1cknvbcGwgjvx4QpvB+leU8dUj2o"
     @date = 'Tue, 27 Mar 2007 19:36:42 +0000'
     @request_type = 'GET'
     @canonical_resource = '/johnsmith/photos/puppy.jpg'
-  
-    @ph = PortableHole.new(@access_key, @secret_key)
+
+    @ph = PortableHole::Request.new(@access_key, @secret_key)
   end
-  
+
   def test_auth_header_string_to_sign
     expected =<<STRING.chomp
 GET
@@ -22,21 +22,31 @@ GET
 Tue, 27 Mar 2007 19:36:42 +0000
 /johnsmith/photos/puppy.jpg
 STRING
-    
-    
-    actual = @ph.string_to_sign(@date, @request_type, @canonical_resource)
-    
-    assert_equal expected, actual
-  end
-  
-  def test_auth_header
-    expected = "Authorization: AWS 0PN5J17HBGZHT7JJ3X82:xXjDGYUmKxnwqr5KXNPGldn5LbA="
 
-    actual = @ph.sign(@date, @request_type, @canonical_resource)
-    
+    actual = @ph.string_to_sign(@date, @request_type, @canonical_resource, '')
+
     assert_equal expected, actual
   end
-  
+
+  def test_string_to_sign_with_content
+
+    expected =<<STRING.chomp
+PUT
+
+image/jpeg
+Tue, 27 Mar 2007 21:15:45 +0000
+/johnsmith/photos/puppy.jpg
+STRING
+
+    date = 'Tue, 27 Mar 2007 21:15:45 +0000'
+    resource = '/johnsmith/photos/puppy.jpg'
+    content_type = 'image/jpeg'
+
+    actual = @ph.string_to_sign(date, 'PUT', resource, content_type)
+
+    assert_equal expected, actual
+  end
+
   # someday/maybe features
   # test_lowercase_request_type
   # test_get
